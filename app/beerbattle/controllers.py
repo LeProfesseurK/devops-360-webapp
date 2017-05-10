e68dff07f168b2c2e02ad2ec8ab2e0eddd296a88
Florian Dambrine <florian@gumgum.com>
2017-05-10 01:41:04 -0700

    Webapp v2.0.0 - Battle + Top-beers features

--- app/beerbattle/controllers.py
+++ app/beerbattle/controllers.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Import flask dependencies
-from flask import Blueprint, render_template, current_app as app
+from flask import Blueprint, render_template, request, jsonify, current_app as app
 
 import json
 
 # Import App objects
-from app.beerbattle.models import Beer
+from app.beerbattle.models import Beer, Battle
 
 # Define a blueprint
 beerbattle = Blueprint('beerbattle', __name__, url_prefix='/')
@@ -25,3 +25,40 @@ def display():
 def catalog():
     app.logger.info('Hit on /catalog endpoint')
     return render_template('catalog.html', beers=Beer.list_beers())
+
+@beerbattle.route('battle', methods=['GET', 'POST'])
+def battle():
+    app.logger.info('Hit on /battle endpoint')
+
+    if request.method == 'POST':
+        json = request.get_json()
+
+        # Convert a list of dict name= value= into a dict
+        json_obj = {}
+        model = None
+
+        for j in json:
+            json_obj[j['name']] = j['value']
+
+        if 'beer_id_1_win' in json_obj and json_obj['beer_id_1_win'] == 'on':
+            model = Battle(json_obj['beer_id_1'], json_obj['beer_id_2'])
+
+        elif 'beer_id_2_win' in json_obj and json_obj['beer_id_2_win'] == 'on':
+            model = Battle(json_obj['beer_id_2'], json_obj['beer_id_1'])
+
+        if model:
+            model.save()
+            return jsonify(result='Battle saved!')
+        else:
+            return jsonify(result='Something went wrong!')
+
+    else:
+        battle_interval = app.config['BEER_BATTLE_ID_INTERVAL'] if 'BEER_BATTLE_ID_INTERVAL' in app.config else None
+        beers_for_battle = Beer.get_two_beers(battle_interval=battle_interval)
+
+        return render_template('battles.html', beers=beers_for_battle)
+
+@beerbattle.route('top-beers', methods=['GET'])
+def top():
+    app.logger.info('Hit on /top-beers endpoint')
+    return render_template('top-beers.html', beers=Battle.list_top_beers())
--- app/beerbattle/models.py
+++ app/beerbattle/models.py
@@ -4,6 +4,7 @@
 from collections import OrderedDict
 
 from random import sample
+from sqlalchemy import ForeignKey, func, or_, desc
 
 from app import db
 
@@ -36,3 +37,77 @@ class Beer(db.Model):
                 brewery = b.brewery,
             )
         return beers
+
+    @staticmethod
+    def get_two_beers(battle_interval=None):
+        beers = {}
+
+        beer_max_id = db.session.query(func.max(Beer.id)).first()[0]
+
+        beer_ids = []
+        max_id = None
+
+        if battle_interval is not None:
+            # Generate two different random ids within the range of beer ids
+            beer_ids = sample(range(
+                max(1, battle_interval[0]),
+                min(beer_max_id, battle_interval[1])), 2)
+
+        records = Beer.query.filter(or_(Beer.id == beer_ids[0], Beer.id == beer_ids[1]))
+
+        for b in records:
+            beers[b.id] = dict(
+                name = b.name,
+                type = b.type,
+                abv = b.abv,
+                brewery = b.brewery,
+            )
+        return beers
+
+class Battle(db.Model):
+    __tablename__ = 'battle'
+
+    battle_id = db.Column(db.Integer, primary_key=True)
+    beer_win_rate = db.Column(db.Integer)
+
+    beer_win_id = db.Column(db.Integer, ForeignKey('beer.id'))
+    beer_lose_id = db.Column(db.Integer, ForeignKey('beer.id'))
+
+    beer_win = db.relationship('Beer', foreign_keys=[beer_win_id],
+        lazy='select')
+
+    beer_lose = db.relationship('Beer', foreign_keys=[beer_lose_id],
+        lazy='select')
+
+    def __init__(self, beer_win_id, beer_lose_id, beer_win_rate=1, battle_id=None):
+        self.battle_id = battle_id
+        self.beer_win_id = beer_win_id
+        self.beer_lose_id = beer_lose_id
+        self.beer_win_rate = beer_win_rate
+
+    def __repr__(self):
+        return '<Battle %r, %r , %r , %r, %r, %r>' % (self.battle_id,
+            self.beer_win_id,
+            self.beer_lose_id,
+            self.beer_win,
+            self.beer_lose,
+            self.beer_win_rate)
+
+    @staticmethod
+    def list_top_beers():
+        beers = OrderedDict()
+
+        records = db.session.query(Battle, func.sum(Battle.beer_win_rate).label('rate')).group_by(Battle.beer_win_id).order_by(desc('rate')).all()
+        for r in records:
+            battle = r[0]
+
+            beers[battle.battle_id] = dict(
+                beer_win = battle.beer_win,
+                rate = r[1]
+            )
+
+        return beers
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
